b787dab5b2478b92531d03790659f1980b86c883
juhaokanyfzx <85215672+juhaokanyfzx@users.noreply.github.com>
2024-09-27 01:56:17 +0000

    delete .output

--- .output/bpftool/bootstrap/json_writer.d
+++
@@ -1,4 +0,0 @@
-/home/github/tool/.output/bpftool/bootstrap/json_writer.o: json_writer.c \
- json_writer.h /home/github/tool/bpftool/include/linux/compiler.h \
- /home/github/tool/bpftool/include/linux/compiler_types.h \
- /home/github/tool/bpftool/include/linux/compiler-gcc.h
